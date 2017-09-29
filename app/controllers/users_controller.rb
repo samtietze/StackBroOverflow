8ca4fdb9a67da8853d39fe7c29ce5e85e9d5a9a9
Sam Tietze <samtietze@gmail.com>
2017-09-28 20:39:12 -0500

    Update users_controller.rb
    @msmele345 remember to add a show.erb. Otherwise all good.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ post '/users' do
   @user = User.new(params[:user])
 
   if @user.save
-    redirect '/users/new'
+    redirect '/questions'
   else
     @errors = @user.errors.full_messages
     erb :"users/new"
