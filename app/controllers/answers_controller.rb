bb5406956276ecc34f273a77f25562be133b9a6d
Charlie Weber <Cweber648@gmail.com>
2017-09-29 07:41:24 -0500

    added controller, seeds, and erbs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,58 @@
 
 get '/questions/:question_id/answers' do
-  # HELPER METHOD TO AUTHENTICATE
+  authenticate!
   @answers = Answers.find_by(id: params[:id])
   erb :'answers/show'
+end
+
+get '/questions/:question_id/answers/new' do
+  authenticate!
+   @question = Question.find(params[:question_id])
+  erb :'answers/new'
+end
+
+post '/questions/:question_id/answers'
+  authenticate!
+  @question = Question.find(params[:question_id])
+  @answer = @question.answers.new(params[:answer])
+    if @answer.save
+      # what here
+      redirect "/questions/#{@question.id}/answers"
+    else
+    @errors = @answer.errors.full_messages
+    # erb "" what here
+    erb :'answers/new'
+    end
+end
+
+get '/questions/:question_id/answers/:id' do
+  @question = Question.find(params[:question_id])
+  @answer = @question.answers.find(params[:id])
+  erb :'answers/show'
+end
 
 
+get '/questions/:question_id/answers/:id/edit' do
+  @question = Question.find(params[:question_id])
+  @answer = @question.answers.find(params[:id])
+  erb :'answers/edit'
+
+end
+
+put '/questions/:question_id/answers/:id' do
+  @question = Question.find(params[:question_id])
+  @answer = @question.answers.find(params[:id])
+  if @answer.update(params[:answer])
+    redirect "/questions/#{@question.id}/answers"
+  else
+    @errors.@answer.errors.full_messages
+    erb :'answers/edit'
+  end
+
+end
+delete '/questions/:question_id/answers/:id' do
+  @answer = Answer.find_by(id: params[:id])
+  authorize!(@answer.user)
+  @answer.destroy
+  redirect  "/questions/#{@question.id}/answers"
 end
