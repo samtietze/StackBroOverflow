094b2d34ef54dcf67efa441d4f0db34582efacac
Sam Tietze <samtietze@gmail.com>
2017-09-28 20:24:18 -0500

    Add links to user profile in summary list

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,17 +3,19 @@ class Question < ApplicationRecord
   has_many :votes, as: :votable
   belongs_to :question_author, class_name: "User"
 
-  def edited?
-    self.updated_at > self.created_at
-  end
 
-  def time_display
-    if edited?
-      self.updated_at
-    else
-      self.created_at
-    end
-  end
+# Editing this out since updating the pageviews "updates" the question.
+  # def edited?
+  #   self.updated_at > self.created_at
+  # end
+
+  # def time_display
+  #   if edited?
+  #     self.updated_at
+  #   else
+  #     self.created_at
+  #   end
+  # end
 
   validates :user_id, :subject, :question_body, { null: false }
   validates :question_body, { length: { maximum: 5000 } }
