6f7c72df78bbc655ccfe4ddee293a36ed7368e0a
Charlie Weber <Cweber648@gmail.com>
2017-09-30 16:59:09 -0500

    added routes for votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,10 +14,53 @@ post '/answers/:answer_id/votes' do
   end
 end
 
+post '/questions/:question_id/votes' do
+  vote_value = params[:vote].to_i
+  @question = Question.find(params[:question_id])
+  @vote = Vote.find_or_initialize_by(voter: current_user, votable: @question)
+  @vote.value = vote_value
+  @vote.save
+  @question.votes << @vote
+
+  if request.xhr?
+    content_type = :json
+    { value: @question.vote_count }.to_json
+  else
+    redirect "/questions/#{@question.id}"
+  end
+
+end
 
 
-    # request.done(function(response){
-    #   var selector = "#" + response.article_id;
-    #   $(selector).find(".points").text(response.votes);
-    #   console.log(response)
-    # });
+post '/questions/:question_id/comments/:id/votes' do
+  vote_value = params[:vote].to_i
+  @comment = Comment.find(params[:comment_id])
+  @vote = Vote.find_or_initialize_by(voter: current_user, votable: @comment)
+  @vote.value = vote_value
+  @vote.save
+  @comment.votes << @vote
+
+  if request.xhr?
+    content_type = :json
+    { value: @answer.vote_count }.to_json
+  else
+    redirect "/questions/#{@comment.question_id}"
+  end
+end
+
+post '/answers/:answer_id/comments/:id/votes' do
+    vote_value = params[:vote].to_i
+  @comment = Comment.find(params[:comment_id])
+  @vote = Vote.find_or_initialize_by(voter: current_user, votable: @comment)
+  @vote.value = vote_value
+  @vote.save
+  @comment.votes << @vote
+
+  if request.xhr?
+    content_type = :json
+    { value: @answer.vote_count }.to_json
+  else
+    redirect "/answers/#{@comment.answer_id}"
+  end
+end
+
