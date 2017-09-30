7899afe0084f42d8f40f10198cdfc93f42850464
Charlie Weber <Cweber648@gmail.com>
2017-09-30 15:48:41 -0500

    added answer routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,58 +6,68 @@
 
 get '/questions/:question_id/answers/new' do
   authenticate!
-   @question = Question.find(params[:question_id])
-   @answer = Answer.new
-    if request.xhr?
-      # partial
-    else
-      erb :'answers/new'
-    end
+  @question = Question.find(params[:question_id])
+  @answer = Answer.new
+  if request.xhr?
+    erb :'answers/new'
+  else
+    erb :'answers/new'
+  end
 end
 
 post '/questions/:question_id/answers' do
   authenticate!
   @question = Question.find(params[:question_id])
   @answer = Answer.new(params[:answer])
-    if @answer.save
-      if request.xhr?
-        # erb :"partial"
-      else
-        redirect "/questions/#{@question.id}"
-      end
+  if @answer.save
+    if request.xhr?
+      erb :'answers/_answer_container', layout: false, locals: { answer: @answer }
     else
-      @errors = @answer.errors.full_messages
-      if request.xhr?
-        status 422
-        erb :"_errors", layout: false, locals: { errors: @errors }
-      else
-        erb :'answers/new'
-      end
+      redirect "/questions/#{@question.id}"
     end
+  else
+    @errors = @answer.errors.full_messages
+    if request.xhr?
+      status 422
+      erb :"_errors", layout: false, locals: { errors: @errors }
+    else
+      erb :'answers/new'
+    end
+  end
 end
 
-get '/questions/:question_id/answers/:id' do
-  @question = Question.find(params[:question_id])
-  @answer = @question.answers.find(params[:id])
-  erb :'answers/show'
-end
+# get '/questions/:question_id/answers/:id' do
+#   @question = Question.find(params[:question_id])
+#   @answer = @question.answers.find(params[:id])
+#   erb :'answers/show'
+# end
 
 
 get '/questions/:question_id/answers/:id/edit' do
-  @question = Question.find(params[:question_id])
-  @answer = @question.answers.find(params[:id])
-  erb :'answers/edit'
-
+  @answer = Answer.find(params[:id])
+  if request.xhr?
+    erb :'answers/edit'
+  else
+    erb :'answers/edit'
+  end
 end
 
 put '/questions/:question_id/answers/:id' do
-  @question = Question.find(params[:question_id])
-  @answer = @question.answers.find(params[:id])
+  @answer = Answer.find(params[:id])
   if @answer.update(params[:answer])
-    redirect "/questions/#{@question.id}/answers"
+    if request.xhr?
+      erb :'answers/_answer_container', layout: false, locals: { answer: @answer }
+    else
+      redirect "/questions/#{@answer.question_id}/answers"
+    end
   else
     @errors = @answer.errors.full_messages
-    erb :'answers/edit'
+    if request.xhr?
+      status 422
+      erb :"_errors", layout: false, locals: { errors: @errors }
+    else
+      erb :'answers/edit'
+    end
   end
 
 end
