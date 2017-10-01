6a2b477a839b46ea05e2f38dac986c66262cbb54
Charlie Weber <Cweber648@gmail.com>
2017-10-01 14:48:30 -0500

    vote buttons are there, but still not adding votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -32,7 +32,7 @@ post '/questions/:question_id/votes' do
 end
 
 
-post '/questions/:question_id/comments/:id/votes' do
+post '/comments/:id/votes' do
   vote_value = params[:vote].to_i
   @comment = Comment.find(params[:comment_id])
   @vote = Vote.find_or_initialize_by(voter: current_user, votable: @comment)
@@ -44,23 +44,27 @@ post '/questions/:question_id/comments/:id/votes' do
     content_type = :json
     { value: @answer.vote_count }.to_json
   else
-    redirect "/questions/#{@comment.question_id}"
+    if @comment.commentable_type = "Question"
+      redirect "/questions/#{@comment.question_id}"
+    else
+      redirect "/questions/#{@comment.answer.question_id}"
+    end
   end
 end
 
-post '/answers/:answer_id/comments/:id/votes' do
-    vote_value = params[:vote].to_i
-  @comment = Comment.find(params[:comment_id])
-  @vote = Vote.find_or_initialize_by(voter: current_user, votable: @comment)
-  @vote.value = vote_value
-  @vote.save
-  @comment.votes << @vote
+# post '/answers/:answer_id/comments/:id/votes' do
+#   vote_value = params[:vote].to_i
+#   @comment = Comment.find(params[:comment_id])
+#   @vote = Vote.find_or_initialize_by(voter: current_user, votable: @comment)
+#   @vote.value = vote_value
+#   @vote.save
+#   @comment.votes << @vote
 
-  if request.xhr?
-    content_type = :json
-    { value: @answer.vote_count }.to_json
-  else
-    redirect "/answers/#{@comment.answer_id}"
-  end
-end
+#   if request.xhr?
+#     content_type = :json
+#     { value: @answer.vote_count }.to_json
+#   else
+#     redirect "/answers/#{@comment.answer_id}"
+#   end
+# end
 
